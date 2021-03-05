1b57daaa5cde090604180e8caf2ccf6e8638ceee
wq <wq>
2021-03-05 15:32:13 +0800

    no message

--- WDWCDBWrapLoader.swift
+++ WDWCDBWrapLoader.swift
@@ -27,14 +27,14 @@ class WDWCDBWrapLoader: NSObject {
     
     //MARK: 插入数据用类名,传表名会使用表名,不传使用tableName()(已经存在会更新)
     @discardableResult
-    func insertToDb<T: FMDBStorageProtocol>(object: T, table: String? = nil) -> Bool {
+    func insertToDb<T: WDDBStorageProtocol>(object: T, table: String? = nil) -> Bool {
         guard let toTable = table ?? object.tableName() else { return false }
         return dbInstance.insertToDb(object: object, intoTable: toTable)
     }
     
     //MARK: 插入数据用类名,传表名会使用表名,不传使用tableName()(已经存在会更新)
     @discardableResult
-    func insertToDbArray<T: FMDBStorageProtocol>(objects: [T], table: String? = nil) -> Bool {
+    func insertToDbArray<T: WDDBStorageProtocol>(objects: [T], table: String? = nil) -> Bool {
         guard let object = objects.first else { return false }
         guard let toTable = table ?? object.tableName() else { return false }
         return dbInstance.insertArrayToDb(objects: objects, intoTable: toTable)
@@ -42,7 +42,7 @@ class WDWCDBWrapLoader: NSObject {
     
     //MARK: 删除数据 where *.Properties.*.is
     @discardableResult
-    func deleteFromDb<T: FMDBStorageProtocol>(ttype: T.Type = T.self, table: String? = nil, where condition: Condition? = nil) -> Bool {
+    func deleteFromDb<T: WDDBStorageProtocol>(ttype: T.Type = T.self, table: String? = nil, where condition: Condition? = nil) -> Bool {
         guard let toTable = table ?? ttype.tableName() else { return false }
         return dbInstance.deleteFromDb(table: toTable, where: condition)
     }
@@ -55,21 +55,21 @@ class WDWCDBWrapLoader: NSObject {
     
     //MARK: 删除全部数据
     @discardableResult
-    func deleteAllFromDb<T: FMDBStorageProtocol>( ttype: T.Type = T.self, table: String? = nil, where condition: Condition? = nil) -> Bool {
+    func deleteAllFromDb<T: WDDBStorageProtocol>( ttype: T.Type = T.self, table: String? = nil, where condition: Condition? = nil) -> Bool {
         guard let toTable = table ?? ttype.tableName() else { return false }
         return dbInstance.deleteFromDb(table: toTable)
     }
 
     //MARK: 查询单个数据 condition查询条件 orderList排序
     @discardableResult
-    func qureyFromDb<T: FMDBStorageProtocol>( ttype: T.Type,  table: String? = nil,  where condition: Condition? = nil) -> T? {
+    func qureyFromDb<T: WDDBStorageProtocol>( ttype: T.Type,  table: String? = nil,  where condition: Condition? = nil) -> T? {
         guard let toTable = table ?? ttype.tableName() else { return nil }
         return dbInstance.qureyFromDb(table: toTable, cls: ttype, where: condition)
     }
 
     //MARK: 查询多个数据  *.Properties.*.asOrder(by: .ascending升序/descending降序)
     @discardableResult
-    func qureyFromDb<T: FMDBStorageProtocol>(
+    func qureyFromDb<T: WDDBStorageProtocol>(
         ttype: T.Type,
         table: String? = nil,
         where condition: Condition? = nil,
